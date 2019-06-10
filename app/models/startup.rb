5dea8b24bac2e3616c921cfc474ec0e60f53f8cd
Danielle <dmiz@brandeis.edu>
2019-06-10 11:07:03 +0100

    Danielle Mizrachi's submission

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,39 @@
 class Startup
+    attr_accessor :name 
+    attr_reader :domain, :founder
 
+    @@all=[]
+
+    def initialize(name:,founder:,domain:)
+        @name=name
+        @founder=founder
+        @domain=domain 
+        @@all<<self 
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    # def pivot(domain,name) 
+    # end 
+    #     #s1.pivot=("new name:" "new domain:")
+
+    def self.find_by_founder(founders_name)
+        founder=@@all.select {|startups| startups.founder==founders_name}
+        founder.first 
+    end 
+
+    def self.domains 
+        @@all.map {|startups| startups.domain}
+    end 
+
+    def num_funding_rounds
+       rounds= FundingRound.all.select {|rounds| self == rounds.startup}
+       rounds.size 
+    end 
+
+    # def total_funds 
+    # rounds= FundingRound.all.select {|rounds| self == rounds.startup}
+    # end 
 end
